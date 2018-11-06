58327cb546def52b38ae3057fd55dedf0cc94352
shawn.l@classbro.com.cn <shawn.l@classbro.com.cn>
2018-11-06 12:43:11 +0800

    初始化提交

---
+++ uikit/src/com/netease/nim/uikit/common/CommonUtil.java
@@ -0,0 +1,35 @@
+package com.netease.nim.uikit.common;
+
+import com.netease.nimlib.sdk.msg.model.RecentContact;
+
+public class CommonUtil {
+
+    public static final String BaseUrl = "http://www.classbro.com/";
+
+
+    public static String getAvatarUrl(String url){
+        if(null == url){
+            return url;
+        }
+        boolean sta = url.startsWith("http://");
+        if(sta){
+            return url;
+        }
+        return CommonUtil.BaseUrl + url;
+    }
+
+
+    public static void addTag(RecentContact recent, long tag) {
+        tag = recent.getTag() | tag;
+        recent.setTag(tag);
+    }
+
+    public static void removeTag(RecentContact recent, long tag) {
+        tag = recent.getTag() & ~tag;
+        recent.setTag(tag);
+    }
+
+    public static boolean isTagSet(RecentContact recent, long tag) {
+        return (recent.getTag() & tag) == tag;
+    }
+}
